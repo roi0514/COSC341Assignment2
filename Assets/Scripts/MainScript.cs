051523e71eb733a1dc5d7fbe748b4d249ee79b1f
eeasuper <42607535+eeasuper@users.noreply.github.com>
2024-07-17 16:20:38 -0700

    Change distance width (#8)
    * Change of radius and distance
    
    Couldn't figure out how to prevent the circles from going out of the screen at 15cm.
    
    * scene settings
    
    * changed GenerateCombinations
    
    made it be a little more readable.
    
    * changed sorting order of Target
    
    the red target now goes on top of any other target that it is overlapping with.

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,72 +1,155 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainScript : MonoBehaviour
 {
-    [SerializeField] float containerDiameter = 4f;
-    [SerializeField] int numberOfCircles = 9;
-    [SerializeField] GameObject circlePrefab;
+
     [SerializeField] GameObject container;
 
-    private List<GameObject> circles;  // this List contains all the circles/targets created
-    private int currentTargetIndex = -1; 
+    [SerializeField] int numberOfCircles = 9;
+    [SerializeField] GameObject circlePrefab;
 
-    // Start is called before the first frame update
+    // this List contains all the circles/targets created
+    private List<GameObject> circles;  
+    private int currentTargetIndex = -1;
+    private int clickedCount;
+
+    private float CAMERA_PROJECT_SIZE_VALUE;
+
+    //these are the values that the prof told us to input divided by the camera...
+    //If the camera screen is square, 5f directly represents 5 cm and 0.5f directly represents 0.5cm (hopefully this is the same on your computer)(pls check if it is).
+    //However I could not figure out how to prevent the circles from going out of the screen - Dup
+    private float scaleRadius = 0.5f;
+    private float scaleDistance = 5f;
+
+    //In the array in each index of combinations, the first index represents the radius. the second index represents the distance.
+    public float[][] combinations;
+
+    //the first round is set as 0
+    private int round;
+
     void Start()
     {
-        container.transform.localScale = containerDiameter * Vector3.one;
-        GenerateCircles();
-        HighlightCurrentTarget();
+        //the value written in the settings.
+        CAMERA_PROJECT_SIZE_VALUE = Camera.main.orthographicSize;
+        scaleRadius = scaleRadius / CAMERA_PROJECT_SIZE_VALUE;
+        scaleDistance = (scaleDistance / CAMERA_PROJECT_SIZE_VALUE);
+
+        GenerateCombinations(scaleRadius,scaleDistance);
+        GenerateCircles(combinations[round][0], combinations[round][1]);
+
+        HighlightStartingCurrentTarget();
     }
 
-    void GenerateCircles()
+    public void GenerateCircles(float radius, float distance)
     {
-        float radius = containerDiameter / 2f;
         circles = new List<GameObject>();
 
         for (int i = 0; i < numberOfCircles; i++)
         {
             float angle = i * (360f / numberOfCircles);
+
             Vector3 position = new Vector3(
-                container.transform.position.x + radius * Mathf.Cos(angle * Mathf.Deg2Rad),
-                container.transform.position.y + radius * Mathf.Sin(angle * Mathf.Deg2Rad),
-                container.transform.position.z
+                //make sure container is positioned at 0.
+                (container.transform.position.x+ Mathf.Cos(angle * Mathf.Deg2Rad)) * distance,
+                (container.transform.position.y+ Mathf.Sin(angle * Mathf.Deg2Rad)) * distance,
+               container.transform.position.z
             );
 
             GameObject circle = Instantiate(circlePrefab, position, Quaternion.identity);
             circle.transform.SetParent(container.transform);
-            circles.Add(circle);
+            circle.transform.localScale = new Vector3(radius, radius, circle.transform.localScale.z);
 
-            
             circle.AddComponent<CircleCollider2D>().isTrigger = true;
             circle.AddComponent<Target>().Initialize(i, OnTargetSelected);
+
+            circles.Add(circle);
+
         }
     }
 
-    void HighlightCurrentTarget()
+    //gives the 9 combinations into combinations[][] 
+    void GenerateCombinations(float scaleRadius, float scaleDistance) {
+        //For a fair evaluation, the particpants should get the same pattern of combinations in the same order. For dynamic change patterns, create a different method.
+        combinations = new float[9][];
+        float[] radiuses = new float[] { scaleRadius*1, scaleRadius*2, scaleRadius*3 };
+        float[] distance = new float[] { scaleDistance*1, scaleDistance * 2, scaleDistance * 3 };
+        int radiusCount = 0;
+        int distanceCount = 0;
+        for (int i = 0; i < numberOfCircles; i++) {
+            //the first index will represent the radius. The second distance.
+            combinations[i] = new float[2];
+            combinations[i][0] = radiuses[radiusCount];
+            combinations[i][1] = distance[distanceCount];
+            distanceCount++;
+            if (distanceCount== distance.Length) {
+                radiusCount++;
+                distanceCount = 0;
+            }
+        }
+        //resulting array if scaleRadius and scaleDistance are equal to 1f: [[1,1],[1,2],[1,3],[2,1],[2,2],...[3,3]]
+        
+    }
+    void HighlightStartingCurrentTarget()
     {
-        if (currentTargetIndex != -1)
+        //Seems to work without this if clause, so I commented it out -- Dup (revert if necessary)
+        /*if (currentTargetIndex != -1)
         {
             circles[currentTargetIndex].GetComponent<SpriteRenderer>().color = Color.white;  
-        }
+        }*/
 
-        currentTargetIndex = Random.Range(0, numberOfCircles);
-        circles[currentTargetIndex].GetComponent<SpriteRenderer>().color = Color.red; 
+        currentTargetIndex = UnityEngine.Random.Range(0, numberOfCircles);
+        circles[currentTargetIndex].GetComponent<SpriteRenderer>().color = Color.red;
+
+        //set the current circle to be above any other circle (so that when it is overlapping with another circle, the current circle comes up
+        circles[currentTargetIndex].GetComponent<SpriteRenderer>().sortingOrder = 1;
+//        circles[currentTargetIndex].gameObject.transform.SetSiblingIndex(0);
     }
 
     void OnTargetSelected(int idx)
     {
         if (idx == currentTargetIndex)
         {
+            clickedCount++;
             Debug.Log($"Circle Selected: Reference - {circles[idx]}, Position ID - {idx}");
 
             circles[currentTargetIndex].GetComponent<SpriteRenderer>().color = Color.white;
 
+            //set the previous circle to a lower sorting order.
+            circles[currentTargetIndex].GetComponent<SpriteRenderer>().sortingOrder = 0;
+            //If the index is at 5, it becomes (5+(9/2))%9 = 0. The next index is set to 0.
             currentTargetIndex = (currentTargetIndex + (numberOfCircles / 2)) % numberOfCircles;
 
             circles[currentTargetIndex].GetComponent<SpriteRenderer>().color = Color.red;
+            circles[currentTargetIndex].GetComponent<SpriteRenderer>().sortingOrder = 1;
         }
+        if(clickedCount > 8) {
+            GoToNextRound();
+            clickedCount = 0;
+        }
+    }
+    public void DestroyList() {
+        foreach (GameObject circle in circles) {
+            Destroy(circle);
+        }
+    }
+    public void GoToNextRound() {
+        round++;
+        if (round > 8) {
+            //end of test
+            DestroyList();
+
+            return;
+        }
+        DestroyList();
+        //Create new circles.
+        GenerateCircles(combinations[round][0], combinations[round][1]);
+        //Highlight random circle.
+        HighlightStartingCurrentTarget();
     }
 }
 
@@ -85,12 +168,11 @@ public class Target : MonoBehaviour
     {
         if (onSelectedCallback != null)
         {
+            //OnTargetSelected is called with idx as its parameter value
             onSelectedCallback.Invoke(idx);
         }
     }
 }
-
-
 
 
 
